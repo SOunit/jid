5d8d67b544eb1ff2d252bb172e20ecd62219e292
SOunit <workbookwork7@gmail.com>
2022-08-23 10:37:14 -0700

    add entities

--- demo/src/com/semanticsquare/oop/interfacedemo/C.java
+++ demo/src/com/semanticsquare/oop/interfacedemo/C.java
@@ -2,4 +2,8 @@ package com.semanticsquare.oop.interfacedemo;
 
 public interface C extends A {
 	void foobar();
+
+	default void go() {
+		System.out.println("go");
+	}
 }
--- demo/src/com/semanticsquare/oop/interfacedemo/TestClient.java
+++ demo/src/com/semanticsquare/oop/interfacedemo/TestClient.java
@@ -9,19 +9,23 @@ public class TestClient {
 	public static void main(String[] args) {
 		System.out.println("Interface------------");
 		// A a = new A();
-		A a = new X();
+//		A a = new X();
+//
+//		a.foo();
+//		a.bar();
+//		((X) a).foobar();
 
-		a.foo();
-		a.bar();
-		((X) a).foobar();
-		
 		System.out.println("Cloneable------------");
 
+//		C c = new X();
+//		C clone = ((X)c).clone();
+//		if(clone != c) {
+//			System.out.println("Clone created!");
+//		}
+
+		System.out.println("Default methods----------");
 		C c = new X();
-		C clone = ((X)c).clone();
-		if(clone != c) {
-			System.out.println("Clone created!");
-		}
+		c.go();
 	}
 
 }
