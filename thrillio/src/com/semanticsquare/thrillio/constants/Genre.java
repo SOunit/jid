5e760ce3854e9f1e8d9d8401ec607118a6727a3e
SOunit <workbookwork7@gmail.com>
2022-08-23 11:29:49 -0700

    add constant classes

---
+++ thrillio/src/com/semanticsquare/thrillio/constants/Genre.java
@@ -0,0 +1,12 @@
+package com.semanticsquare.thrillio.constants;
+
+public class Genre {
+	private Genre() {
+
+	}
+
+	public static final int MALE = 0;
+	public static final int FEMALE = 1;
+	public static final int TRANSGENDER = 2;
+
+}
